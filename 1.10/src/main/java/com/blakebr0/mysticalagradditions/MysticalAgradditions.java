b8acbee3ebbad881f88354b16a0dbde19c9a2086
BlakeBr0 <luasmrks@gmail.com>
2017-03-17 20:44:28 -0500

    Some stuff

--- 1.10/src/main/java/com/blakebr0/mysticalagradditions/MysticalAgradditions.java
+++ 1.10/src/main/java/com/blakebr0/mysticalagradditions/MysticalAgradditions.java
@@ -1,6 +1,14 @@
 package com.blakebr0.mysticalagradditions;
 
+import com.blakebr0.mysticalagradditions.proxy.CommonProxy;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = MysticalAgradditions.MOD_ID, name = MysticalAgradditions.NAME, version = MysticalAgradditions.VERSION)
 public class MysticalAgradditions {
@@ -9,5 +17,25 @@ public class MysticalAgradditions {
 	public static final String NAME = "Mystical Agradditions";
 	public static final String VERSION = "1.0.0";
 	
-		
+	@Instance(MOD_ID)
+	public static MysticalAgradditions instance;
+	
+	@SidedProxy(clientSide = "com.blakebr0.mysticalagradditions.proxy.ClientProxy",
+				serverSide = "com.blakebr0.mysticalagradditions.proxy.ServerProxy")
+	public static CommonProxy proxy;
+	
+	@SubscribeEvent
+	public void preInit(FMLPreInitializationEvent event){
+		proxy.preInit(event);
+	}
+	
+	@SubscribeEvent
+	public void init(FMLInitializationEvent event){
+		proxy.init(event);
+	}
+	
+	@SubscribeEvent
+	public void postInit(FMLPostInitializationEvent event){
+		proxy.postInit(event);
+	}
 }
--- 1.10/src/main/java/com/blakebr0/mysticalagradditions/proxy/CommonProxy.java
+++ 1.10/src/main/java/com/blakebr0/mysticalagradditions/proxy/CommonProxy.java
@@ -1,5 +1,20 @@
 package com.blakebr0.mysticalagradditions.proxy;
 
-public class CommonProxy {
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+public class CommonProxy {
+	
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	public void init(FMLInitializationEvent event){
+		
+	}
+
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
 }
