13fa48265bd9df2decbac44bd37afe2d294c3ea7
BlakeBr0 <luasmrks@gmail.com>
2017-03-17 20:09:17 -0500

    Create repo

---
+++ 1.10/src/main/java/com/blakebr0/mysticalagradditions/MysticalAgradditions.java
@@ -0,0 +1,13 @@
+package com.blakebr0.mysticalagradditions;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = MysticalAgradditions.MOD_ID, name = MysticalAgradditions.NAME, version = MysticalAgradditions.VERSION)
+public class MysticalAgradditions {
+
+	public static final String MOD_ID = "mysticalagradditions";
+	public static final String NAME = "Mystical Agradditions";
+	public static final String VERSION = "1.0.0";
+	
+		
+}
---
+++ 1.10/src/main/java/com/blakebr0/mysticalagradditions/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package com.blakebr0.mysticalagradditions.proxy;
+
+public class ClientProxy {
+
+}
