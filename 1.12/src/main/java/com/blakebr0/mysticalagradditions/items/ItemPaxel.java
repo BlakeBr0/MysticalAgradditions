02869e185340fe79d0da17840afc4610874a0bb3
BlakeBr0 <luasmrks@gmail.com>
2017-10-07 13:30:13 -0500

    Fix https://github.com/BlakeBr0/MysticalAgradditions/issues/15

--- 1.12/src/main/java/com/blakebr0/mysticalagradditions/items/ItemPaxel.java
+++ 1.12/src/main/java/com/blakebr0/mysticalagradditions/items/ItemPaxel.java
@@ -18,6 +18,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.nbt.NBTTagCompound;
@@ -65,6 +66,48 @@ public class ItemPaxel extends ItemTool implements IRepairMaterial {
         	   && material != Material.WOOD && material != Material.PLANTS && material != Material.VINE 
         	   ? super.getStrVsBlock(stack, state) : this.efficiencyOnProperMaterial;
 	}
+	
+	@Override
+    public boolean canHarvestBlock(IBlockState state) {
+        Block block = state.getBlock();
+
+        if (block == Blocks.OBSIDIAN) {
+            return this.toolMaterial.getHarvestLevel() == 3;
+        } else if (block != Blocks.DIAMOND_BLOCK && block != Blocks.DIAMOND_ORE) {
+            if (block != Blocks.EMERALD_ORE && block != Blocks.EMERALD_BLOCK) {
+                if (block != Blocks.GOLD_BLOCK && block != Blocks.GOLD_ORE) {
+                    if (block != Blocks.IRON_BLOCK && block != Blocks.IRON_ORE) {
+                        if (block != Blocks.LAPIS_BLOCK && block != Blocks.LAPIS_ORE) {
+                            if (block != Blocks.REDSTONE_ORE && block != Blocks.LIT_REDSTONE_ORE) {
+                                Material material = state.getMaterial();
+
+                                if (material == Material.ROCK) {
+                                    return true;
+                                }
+                                else if (material == Material.IRON) {
+                                    return true;
+                                } else {
+                                    return material == Material.ANVIL;
+                                }
+                            } else {
+                                return this.toolMaterial.getHarvestLevel() >= 2;
+                            }
+                        } else {
+                            return this.toolMaterial.getHarvestLevel() >= 1;
+                        }
+                    } else {
+                        return this.toolMaterial.getHarvestLevel() >= 1;
+                    }
+                } else {
+                    return this.toolMaterial.getHarvestLevel() >= 2;
+                }
+            } else {
+                return this.toolMaterial.getHarvestLevel() >= 2;
+            }
+        } else {
+            return this.toolMaterial.getHarvestLevel() >= 2;
+        }
+    }
 	
 	@Override
     public boolean getIsRepairable(ItemStack toRepair, ItemStack repair){
