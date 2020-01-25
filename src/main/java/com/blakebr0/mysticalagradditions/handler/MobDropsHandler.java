c659113237180f87698148cad31a17ecea5e8ee8
BlakeBr0 <luasmrks@gmail.com>
2020-01-25 01:59:05 -0600

    Fix compile errors

--- src/main/java/com/blakebr0/mysticalagradditions/handler/MobDropsHandler.java
+++ src/main/java/com/blakebr0/mysticalagradditions/handler/MobDropsHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,16 +19,17 @@ public class MobDropsHandler {
 	public void onLivingDrops(LivingDropsEvent event) {
 		LivingEntity entity = event.getEntityLiving();
 		World world = entity.getEntityWorld();
+		BlockPos pos = entity.getPosition();
 		Collection<ItemEntity> drops = event.getDrops();
 		Double witheringSoulChance = ModConfigs.WITHERING_SOUL_DROP_CHANCE.get();
 		Integer dragonScalesAmount = ModConfigs.DRAGON_SCALES_AMOUNT.get();
 
 		if (entity instanceof WitherEntity && Math.random() < witheringSoulChance) {
-			drops.add(new ItemEntity(world, entity.posX, entity.posY, entity.posZ, new ItemStack(ModItems.WITHERING_SOUL.get())));
+			drops.add(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(ModItems.WITHERING_SOUL.get())));
 		}
 
 		if (entity instanceof EnderDragonEntity && dragonScalesAmount > 0) {
-			drops.add(new ItemEntity(world, entity.posX, entity.posY, entity.posZ, new ItemStack(ModItems.DRAGON_SCALE.get(), dragonScalesAmount)));
+			drops.add(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(ModItems.DRAGON_SCALE.get(), dragonScalesAmount)));
 		}
 	}
 }
