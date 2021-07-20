1d52d1b985d7ed5d871c30d31fd556b1923bc2d7
BlakeBr0 <luasmrks@gmail.com>
2021-07-20 00:13:07 -0500

    Update tconstruct modifiers to new api

--- src/main/java/com/blakebr0/mysticalagradditions/compat/tconstruct/modifier/ProsperousModifier.java
+++ src/main/java/com/blakebr0/mysticalagradditions/compat/tconstruct/modifier/ProsperousModifier.java
@@ -1,7 +1,6 @@
 package com.blakebr0.mysticalagradditions.compat.tconstruct.modifier;
 
 import com.blakebr0.mysticalagriculture.api.MysticalAgricultureAPI;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.Item;
@@ -9,8 +8,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.registries.ForgeRegistries;
 import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.context.ToolHarvestContext;
 import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
 
 public class ProsperousModifier extends Modifier {
@@ -19,23 +21,30 @@ public class ProsperousModifier extends Modifier {
     }
 
     @Override
-    public int afterLivingHit(IModifierToolStack tool, int level, LivingEntity attacker, LivingEntity target, float damageDealt, boolean isCritical, float cooldown) {
-        World world = target.getCommandSenderWorld();
+    public int afterEntityHit(IModifierToolStack tool, int level, ToolAttackContext context, float damageDealt) {
+        LivingEntity target = context.getLivingTarget();
 
-        if (!target.isAlive() && !world.isClientSide() && world.getRandom().nextFloat() < 0.005F * level) {
-            Item shard = ForgeRegistries.ITEMS.getValue(new ResourceLocation(MysticalAgricultureAPI.MOD_ID, "prosperity_shard"));
-            ItemEntity item = new ItemEntity(world, target.getX() + 0.5, target.getY(), target.getZ() + 0.5, new ItemStack(shard));
+        if (target != null) {
+            World world = target.getCommandSenderWorld();
 
-            world.addFreshEntity(item);
+            if (!target.isAlive() && !world.isClientSide() && world.getRandom().nextFloat() < 0.005F * level) {
+                Item shard = ForgeRegistries.ITEMS.getValue(new ResourceLocation(MysticalAgricultureAPI.MOD_ID, "prosperity_shard"));
+                ItemEntity item = new ItemEntity(world, target.getX() + 0.5, target.getY(), target.getZ() + 0.5, new ItemStack(shard));
+
+                world.addFreshEntity(item);
+            }
         }
 
-        return super.afterLivingHit(tool, level, attacker, target, damageDealt, isCritical, cooldown);
+        return super.afterEntityHit(tool, level, context, damageDealt);
     }
 
     @Override
-    public void afterBlockBreak(IModifierToolStack tool, int level, World world, BlockState state, BlockPos pos, LivingEntity target, boolean canHarvest, boolean wasEffective) {
-        if (wasEffective && !world.isClientSide() && world.getRandom().nextFloat() < 0.005F * level) {
+    public void afterBlockBreak(IModifierToolStack tool, int level, ToolHarvestContext context) {
+        ServerWorld world = context.getWorld();
+
+        if (context.isEffective() && !world.isClientSide() && world.getRandom().nextFloat() < 1.005F * level) {
             Item shard = ForgeRegistries.ITEMS.getValue(new ResourceLocation(MysticalAgricultureAPI.MOD_ID, "prosperity_shard"));
+            BlockPos pos = context.getPos();
             ItemEntity item = new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(shard));
 
             world.addFreshEntity(item);
