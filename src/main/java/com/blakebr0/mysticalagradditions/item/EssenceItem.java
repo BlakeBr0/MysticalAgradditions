122ce28127df53f0723dd6f9383f94687cdd168c
BlakeBr0 <luasmrks@gmail.com>
2021-12-12 20:09:01 -0600

    Update for FarmlandConverter changes

--- src/main/java/com/blakebr0/mysticalagradditions/item/EssenceItem.java
+++ src/main/java/com/blakebr0/mysticalagradditions/item/EssenceItem.java
@@ -4,6 +4,7 @@ import com.blakebr0.cucumber.item.BaseItem;
 import com.blakebr0.mysticalagradditions.config.MAConfig;
 import com.blakebr0.mysticalagriculture.api.crop.CropTier;
 import com.blakebr0.mysticalagriculture.api.crop.ICropTierProvider;
+import com.blakebr0.mysticalagriculture.api.farmland.FarmlandConverter;
 import com.blakebr0.mysticalagriculture.api.farmland.IFarmlandConverter;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.context.UseOnContext;
@@ -24,7 +25,7 @@ public class EssenceItem extends BaseItem implements IFarmlandConverter, ICropTi
         if (!MAConfig.getEssenceFarmlandConversion())
             return InteractionResult.PASS;
 
-        return this.convert(context);
+        return FarmlandConverter.convert(this, context);
     }
 
     @Override
