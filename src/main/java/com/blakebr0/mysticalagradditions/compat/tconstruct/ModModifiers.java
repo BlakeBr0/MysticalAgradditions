47620bda4a8d87e944ac2f92455da0839938f6a9
BlakeBr0 <luasmrks@gmail.com>
2022-07-04 14:01:44 -0500

    Fix tinkers' construct integration issues closes #88

--- src/main/java/com/blakebr0/mysticalagradditions/compat/tconstruct/ModModifiers.java
+++ src/main/java/com/blakebr0/mysticalagradditions/compat/tconstruct/ModModifiers.java
@@ -3,13 +3,13 @@ package com.blakebr0.mysticalagradditions.compat.tconstruct;
 import com.blakebr0.mysticalagradditions.MysticalAgradditions;
 import com.blakebr0.mysticalagradditions.compat.tconstruct.modifier.ProsperousModifier;
 import com.blakebr0.mysticalagradditions.compat.tconstruct.modifier.SoulSiphonerModifier;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
 import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
+import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
 
 public final class ModModifiers {
-    public static final DeferredRegister<Modifier> REGISTRY = DeferredRegister.create(Modifier.class, MysticalAgradditions.MOD_ID);
+    public static final ModifierDeferredRegister REGISTRY = ModifierDeferredRegister.create(MysticalAgradditions.MOD_ID);
 
-    public static final RegistryObject<Modifier> PROSPEROUS = REGISTRY.register("prosperous", ProsperousModifier::new);
-    public static final RegistryObject<Modifier> SOUL_SIPHONER = REGISTRY.register("soul_siphoner", SoulSiphonerModifier::new);
+    public static final StaticModifier<Modifier> PROSPEROUS = REGISTRY.register("prosperous", ProsperousModifier::new);
+    public static final StaticModifier<Modifier> SOUL_SIPHONER = REGISTRY.register("soul_siphoner", SoulSiphonerModifier::new);
 }
