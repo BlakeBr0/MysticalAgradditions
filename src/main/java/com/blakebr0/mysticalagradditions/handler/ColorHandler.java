46e46312616a4b3faf6a5b723feddfb9553db387
BlakeBr0 <luasmrks@gmail.com>
2022-07-23 20:08:24 -0500

    Update to latest forge

--- src/main/java/com/blakebr0/mysticalagradditions/handler/ColorHandler.java
+++ src/main/java/com/blakebr0/mysticalagradditions/handler/ColorHandler.java
@@ -2,17 +2,17 @@ package com.blakebr0.mysticalagradditions.handler;
 
 import com.blakebr0.cucumber.iface.IColored;
 import com.blakebr0.mysticalagradditions.block.InfusedFarmlandBlock;
-import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public final class ColorHandler {
     @SubscribeEvent
-    public void onBlockColors(ColorHandlerEvent.Block event) {
-        event.getBlockColors().register(new IColored.BlockColors(), InfusedFarmlandBlock.FARMLANDS.toArray(new InfusedFarmlandBlock[0]));
+    public void onBlockColors(RegisterColorHandlersEvent.Block event) {
+        event.register(new IColored.BlockColors(), InfusedFarmlandBlock.FARMLANDS.toArray(new InfusedFarmlandBlock[0]));
     }
 
     @SubscribeEvent
-    public void onItemColors(ColorHandlerEvent.Item event) {
-        event.getItemColors().register(new IColored.ItemBlockColors(), InfusedFarmlandBlock.FARMLANDS.toArray(new InfusedFarmlandBlock[0]));
+    public void onItemColors(RegisterColorHandlersEvent.Item event) {
+        event.register(new IColored.ItemBlockColors(), InfusedFarmlandBlock.FARMLANDS.toArray(new InfusedFarmlandBlock[0]));
     }
 }
