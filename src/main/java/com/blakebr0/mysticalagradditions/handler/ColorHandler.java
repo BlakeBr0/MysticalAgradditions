066065e52231b154c249d6ab9101f317241fdd67
BlakeBr0 <luasmrks@gmail.com>
2022-08-07 11:42:44 -0500

    Fix bucket fluid colors

--- src/main/java/com/blakebr0/mysticalagradditions/handler/ColorHandler.java
+++ src/main/java/com/blakebr0/mysticalagradditions/handler/ColorHandler.java
@@ -2,7 +2,11 @@ package com.blakebr0.mysticalagradditions.handler;
 
 import com.blakebr0.cucumber.iface.IColored;
 import com.blakebr0.mysticalagradditions.block.InfusedFarmlandBlock;
+import com.blakebr0.mysticalagradditions.init.ModItems;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public final class ColorHandler {
@@ -14,5 +18,19 @@ public final class ColorHandler {
     @SubscribeEvent
     public void onItemColors(RegisterColorHandlersEvent.Item event) {
         event.register(new IColored.ItemBlockColors(), InfusedFarmlandBlock.FARMLANDS.toArray(new InfusedFarmlandBlock[0]));
+
+        var buckets = new Item[] {
+                ModItems.MOLTEN_INFERIUM_BUCKET.get(),
+                ModItems.MOLTEN_PRUDENTIUM_BUCKET.get(),
+                ModItems.MOLTEN_TERTIUM_BUCKET.get(),
+                ModItems.MOLTEN_IMPERIUM_BUCKET.get(),
+                ModItems.MOLTEN_SUPREMIUM_BUCKET.get(),
+                ModItems.MOLTEN_SOULIUM_BUCKET.get()
+        };
+
+        event.register((stack, tint) -> {
+            var fluid = ((BucketItem) stack.getItem()).getFluid();
+            return tint == 1 ? IClientFluidTypeExtensions.of(fluid).getTintColor() : -1;
+        }, buckets);
     }
 }
