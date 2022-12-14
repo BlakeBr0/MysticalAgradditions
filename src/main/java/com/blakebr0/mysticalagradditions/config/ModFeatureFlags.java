533113c28fde9b0f5e72756c9c3f5dbbdc627391
BlakeBr0 <luasmrks@gmail.com>
2022-12-13 22:54:20 -0600

    Remove HIDE_UNUSED_CRUXES config option

--- src/main/java/com/blakebr0/mysticalagradditions/config/ModFeatureFlags.java
+++ src/main/java/com/blakebr0/mysticalagradditions/config/ModFeatureFlags.java
@@ -13,7 +13,6 @@ public final class ModFeatureFlags {
     public static final FeatureFlag GENERATE_END_PROSPERITY = FeatureFlag.create(new ResourceLocation(MysticalAgradditions.MOD_ID, "generate_end_prosperity"), ModConfigs.GENERATE_END_PROSPERITY);
     public static final FeatureFlag GENERATE_NETHER_INFERIUM = FeatureFlag.create(new ResourceLocation(MysticalAgradditions.MOD_ID, "generate_nether_inferium"), ModConfigs.GENERATE_NETHER_INFERIUM);
     public static final FeatureFlag GENERATE_NETHER_PROSPERITY = FeatureFlag.create(new ResourceLocation(MysticalAgradditions.MOD_ID, "generate_nether_prosperity"), ModConfigs.GENERATE_NETHER_PROSPERITY);
-    public static final FeatureFlag HIDE_UNUSED_CRUXES = FeatureFlag.create(new ResourceLocation(MysticalAgradditions.MOD_ID, "hide_unused_cruxes"), ModConfigs.HIDE_UNUSED_CRUXES);
 
     public static final FeatureFlag ENCHANTABLE_SUPREMIUM_TOOLS = FeatureFlag.from(new ResourceLocation(MysticalAgricultureAPI.MOD_ID, "enchantable_supremium_tools"));
     public static final FeatureFlag ESSENCE_FARMLAND_CONVERSION = FeatureFlag.from(new ResourceLocation(MysticalAgricultureAPI.MOD_ID, "essence_farmland_conversion"));
