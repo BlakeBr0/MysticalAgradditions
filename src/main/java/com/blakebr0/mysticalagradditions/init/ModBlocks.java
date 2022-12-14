5935dae70fab8bfb902cecaa4c59ab7211a25be2
BlakeBr0 <luasmrks@gmail.com>
2022-12-13 22:06:22 -0600

    Add Gaia Spirit Seeds (#91)
    * Start adding gaia spirit seeds
    
    * Remove unnecessary TODO
    
    * Update ru_ru.lang (#93)
    
    * Add tools, ingots, nuggets, gems and storage blocks to their corresponding forge tags
    
    * Release 6.0.1
    
    * Textures and fix crop ingredient
    
    Co-authored-by: Bytegm <45900969+Bytegm@users.noreply.github.com>

--- src/main/java/com/blakebr0/mysticalagradditions/init/ModBlocks.java
+++ src/main/java/com/blakebr0/mysticalagradditions/init/ModBlocks.java
@@ -47,6 +47,7 @@ public final class ModBlocks {
 	public static final RegistryObject<Block> END_INFERIUM_ORE = register("end_inferium_ore", () -> new BaseOreBlock(Material.STONE, SoundType.STONE, 3.0F, 3.0F, 2, 5));
 	public static final RegistryObject<Block> NETHER_STAR_CRUX = register("nether_star_crux", () -> new CruxBlock(ModCorePlugin.NETHER_STAR));
 	public static final RegistryObject<Block> DRAGON_EGG_CRUX = register("dragon_egg_crux", () -> new CruxBlock(ModCorePlugin.DRAGON_EGG));
+	public static final RegistryObject<Block> GAIA_SPIRIT_CRUX = register("gaia_spirit_crux", () -> new CruxBlock(ModCorePlugin.GAIA_SPIRIT));
 	public static final RegistryObject<Block> AWAKENED_DRACONIUM_CRUX = register("awakened_draconium_crux", () -> new CruxBlock(ModCorePlugin.AWAKENED_DRACONIUM));
 	public static final RegistryObject<Block> NITRO_CRYSTAL_CRUX = register("nitro_crystal_crux", () -> new CruxBlock(ModCorePlugin.NITRO_CRYSTAL));
 
